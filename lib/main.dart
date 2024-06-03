677ff8e6ce9651dc9696ec723e3b21eb3c69de38
hamezksm <jamcav097@gmail.com>
2024-06-03 13:01:55 +0300

    Remove dotenv. i will never use it again

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:foodapp/app.dart';
 import 'package:foodapp/core/services/database/database_service.dart';
 import 'package:foodapp/core/services/network/api_service.dart';
@@ -11,22 +7,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  const bool isProduction = bool.fromEnvironment('dart.vm.product');
-  if (!isProduction) {
-    await dotenv.load(fileName: '.env');
-  }
+  const apiKey = String.fromEnvironment('API_KEY');
 
-  final apiKey = isProduction
-      ? const String.fromEnvironment('API_KEY')
-      : dotenv.env['API_KEY']!;
-
-  if (apiKey.isEmpty) {
-    log('API key not found!');
-  } else {
-    // Store the API key in SharedPreferences
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('API_KEY', apiKey);
-  }
+  // Store the API key in SharedPreferences
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.setString('API_KEY', apiKey);
 
   isarService;
   apiService.initialize();
